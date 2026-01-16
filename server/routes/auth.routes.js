1322b581b3311d3e3dc14ab2f9f3cd4f77cd9209
Maxime Ducamp <maxime.ducamp@gmail.com>
2026-01-16 09:44:14 +0100

    typo err

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -10,22 +10,22 @@ router.post("/signup", async (req, res, next) => {
     const { email, password, userName } = req.body;
     const existingUser = await User.findOne({ email });
     if (existingUser) {
-      res.statut(403).json({errorMessage:"Email already taken"});
+      res.status(403).json({ errorMessage: "Email already taken" });
     } else {
       const theSalt = bcryptjs.genSaltSync(12);
       const hashedPassword = bcryptjs.hashSync(password, theSalt);
       const hashedUser = {
         ...req.body,
-        password: hashedPassword
-      }
+        password: hashedPassword,
+      };
       const newUser = await User.create(hashedUser);
-      res.status(201).json(newUser);}
+      res.status(201).json(newUser);
+    }
   } catch (error) {
     next(error);
   }
 });
 
 // route to login an existing user
-
 
 module.exports = router;
